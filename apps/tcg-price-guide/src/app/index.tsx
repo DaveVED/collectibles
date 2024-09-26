3a7522f9c277726246aff6dd601e57b42ef87d20
DaveVED <dave.w.dennis@gmail.com>
2024-09-26 11:09:15 -0500

    feat: basic fetcher setup for exmaple.

--- apps/tcg-price-guide/src/app/index.tsx
+++ apps/tcg-price-guide/src/app/index.tsx
@@ -1,6 +1,14 @@
 import React from "react";
+import useSWR from 'swr';
+
+const fetcher = (url: string) => fetch(url).then(res => res.json());
 
 function App(): JSX.Element {
+  const { data, error, isLoading } = useSWR("http://localhost:5001/status", fetcher);
+  
+  if (error) return <div>failed to load</div>
+  if (isLoading) return <div>loading...</div>
+
   return (
     <div className="min-h-screen flex flex-col items-center justify-center gap-6 text-center mx-auto px-4">
       <h1 className="text-6xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-red-500">
@@ -8,6 +16,8 @@ function App(): JSX.Element {
       </h1>
       <p className="text-gray-500 font-medium">
         This is a basic setup with Tailwind CSS!
+        <br></br>
+        {JSON.stringify(data)}
       </p>
     </div>
   );
